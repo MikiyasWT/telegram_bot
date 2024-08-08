189fccc57534cbba1693a109851687e6f4d39c68
MikiyasWT <mikiywendu44@gmail.com>
2024-08-08 10:19:27 +0300

    bug fix

--- index.py
+++ index.py
@@ -54,5 +54,8 @@ def webhook():
     return 'OK', 200
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
